1f122dea7dec70702bf1b0dac5068cf6ade027c1
Trishan Wagle <mailtotrishan@gmail.com>
2024-06-18 18:24:17 +0545

    feat: create poll form component

---
+++ src/lib/Button.svelte
@@ -0,0 +1,46 @@
+<script lang="ts">
+  export let type: "primary" | "secondary" = "primary";
+  export let flat: boolean = false;
+  export let inverse: boolean = false;
+</script>
+
+<button class:flat class:inverse class={type} on:click>
+  <slot></slot>
+</button>
+
+<style>
+  button {
+    border: 0;
+    cursor: pointer;
+    border-radius: 6px;
+    padding: 8px 12px;
+    font-weight: bold;
+    box-shadow: 1px 2px 3px rgba(0, 0, 0, 0.2);
+  }
+
+  .flat {
+    box-shadow: none;
+  }
+
+  .primary {
+    background: #d91b42;
+    color: white;
+  }
+
+  .secondary {
+    background: #45c496;
+    color: white;
+  }
+
+  .primary.inverse {
+    color: #d91b42;
+    background: white;
+    border: 2px solid #d91b42;
+  }
+
+  .secondary.inverse {
+    color: #45c496;
+    background: white;
+    border: 2px solid #45c496;
+  }
+</style>
--- src/lib/PollForm.svelte
+++ src/lib/PollForm.svelte
@@ -1,8 +1,56 @@
-<script lang="ts"></script>
+<script lang="ts">
+  import Button from "./Button.svelte";
 
-<div>
-  <p>New Poll Form Component goes here</p>
-</div>
+  let formFields = {
+    question: "",
+    answerA: "",
+    answerB: "",
+  };
+
+  const onAddPoll = () => {
+    console.log(formFields);
+  };
+</script>
+
+<form on:submit|preventDefault={onAddPoll}>
+  <div class="form-field">
+    <label for="question">Poll Question:</label>
+    <input type="text" id="question" bind:value={formFields.question} />
+  </div>
+
+  <div class="form-field">
+    <label for="ans-a">Answer A:</label>
+    <input type="text" id="ans-a" bind:value={formFields.answerA} />
+  </div>
+
+  <div class="form-field">
+    <label for="ans-b">Answer B:</label>
+    <input type="text" id="ans-b" bind:value={formFields.answerB} />
+  </div>
+
+  <Button type="secondary">Add Poll</Button>
+</form>
 
 <style>
+  form {
+    width: 400px;
+    margin: 0 auto;
+    text-align: center;
+  }
+
+  .form-field {
+    margin: 18px auto;
+  }
+
+  input {
+    width: 100%;
+    border-radius: 6px;
+    padding: 10px 8px;
+    border: 1px solid #ddd;
+  }
+
+  label {
+    margin: 10px auto;
+    text-align: left;
+  }
 </style>
