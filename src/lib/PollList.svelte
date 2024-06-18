67a3551ee7e26fb8ba7d8bf27612d53099bf501a
Trishan Wagle <mailtotrishan@gmail.com>
2024-06-18 22:21:22 +0545

    feat: polls list component setup

--- src/lib/PollList.svelte
+++ src/lib/PollList.svelte
@@ -1,8 +1,19 @@
-<script lang="ts"></script>
+<script lang="ts">
+  import { pollsStore } from "../stores";
+</script>
 
-<div>
-  <p>Poll List Component goes here</p>
+<div class="poll-list">
+  {#each $pollsStore as poll (poll.id)}
+    <div>{poll.question}</div>
+  {:else}
+    <p>No any polls to show...</p>
+  {/each}
 </div>
 
 <style>
+  .poll-list {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    grid-gap: 20px;
+  }
 </style>
